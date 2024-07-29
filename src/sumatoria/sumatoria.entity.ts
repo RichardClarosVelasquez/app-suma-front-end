ba7ceab00d392ed821b2a753fe9572fbacc6d80f
RichardClarosVelasquez <richard.claros@gmail.com>
2024-07-28 23:23:45 -0400

    Se corrigio conexion desde el back-end al front-end

--- src/sumatoria/sumatoria.entity.ts
+++ src/sumatoria/sumatoria.entity.ts
@@ -3,7 +3,7 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class Sumatoria {
     @PrimaryGeneratedColumn()
-    id: number;
+    id?: number;
     @Column()
     numero1: number;
     @Column()
